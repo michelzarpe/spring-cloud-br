a121264fd8869886c07920e8f033c31b37b5328a
Michel Zarpelon <michelzarpe@gmail.com>
2020-11-06 17:48:29 -0300

    Feign Client

--- hr-payroll/src/main/java/com/sancon/hrpayroll/operation/PaymentService.java
+++ hr-payroll/src/main/java/com/sancon/hrpayroll/operation/PaymentService.java
@@ -1,12 +1,9 @@
 package com.sancon.hrpayroll.operation;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+
+import com.sancon.hrpayroll.feignclients.WorkerFeignClient;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,16 +12,24 @@ import lombok.extern.slf4j.Slf4j;
 public class PaymentService {
 
 	@Autowired
-	private RestTemplate restTemplate;
-	@Autowired
-	private Environment env;
+	private WorkerFeignClient workerFeignClient;
+	//private RestTemplate restTemplate;
+	
+	//@Autowired
+	//private Environment env;
 	
 	public Payment getPayment(long workerId, int days) {
-		String workerHost = env.getProperty("hr-worker.host");
-		log.warn(String.format("HOST Worker: %s", workerHost));
-		Map<String,String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerId);
-		Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class,uriVariables);
+		log.warn("Iniciando.....Payment");
+		//RestTemplat
+		//String workerHost = env.getProperty("hr-worker.host");
+		//log.warn(String.format("HOST Worker: %s", workerHost));
+		//Map<String,String> uriVariables = new HashMap<>();
+		//uriVariables.put("id", ""+workerId);
+		//Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class,uriVariables);
+		
+		//FeignClients
+		Worker worker = workerFeignClient.findAll(workerId).getBody();
+		
 		log.warn("Retornando.....Payment");
 		return new Payment(1L, worker.getName(), worker.getDailyIncome(), days);
 	}
