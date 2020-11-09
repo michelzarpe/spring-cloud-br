83724580eba94cc47712c17688f06fda8d34a97e
Michel Zarpelon <michelzarpe@gmail.com>
2020-11-09 16:33:55 -0300

    private git repository

--- hr-config-server/src/main/java/com/sancon/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/sancon/hrconfigserver/HrConfigServerApplication.java
@@ -1,15 +1,30 @@
 package com.sancon.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.core.env.Environment;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
+public class HrConfigServerApplication implements CommandLineRunner{
 
+	@Autowired
+	private Environment env;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info(env.getProperty("spring.cloud.config.server.git.username"));
+		log.info(env.getProperty("spring.cloud.config.server.git.password"));
 	}
 
 }
