be27068dd41a172c4352d07cf26ec13676a00d5a
Michel Zarpelon <michelzarpe@gmail.com>
2020-11-09 14:52:56 -0300

    Project hr-config-server created

---
+++ hr-config-server/src/test/java/com/sancon/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sancon.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
