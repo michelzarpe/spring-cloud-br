1c6f50a58b500537b69458f0a41fa32f93ec31a3
Michel Zarpelon <michelzarpe@gmail.com>
2020-11-10 17:40:59 -0300

    Login, jwt token

--- hr-oauth/src/main/java/com/sancon/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/sancon/hroauth/services/UserService.java
@@ -1,6 +1,9 @@
 package com.sancon.hroauth.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.sancon.hroauth.entities.User;
@@ -10,7 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	@Autowired
 	private UserFeignClient userFeignClient;
@@ -22,6 +25,17 @@ public class UserService {
 			throw new IllegalArgumentException("Email not Found");
 		}
 		log.info("Email found: "+email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if(user ==null) {
+			log.error("Email not found: "+username);
+			throw new UsernameNotFoundException("Email not Found");
+		}
+		log.info("Email found: "+username);
 		return user;
 	}
 }
